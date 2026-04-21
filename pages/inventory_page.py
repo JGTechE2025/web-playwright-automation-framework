edceea80bad593fede44739b65c2a6302b1de560
Joe <joegou721@gmail.com>
2026-04-21 11:08:14 +0800

    refactor: 調整元素定位方式改為 ID

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -4,7 +4,7 @@ from pages.base_page import BasePage
 class InventoryPage(BasePage):
     def add_first_item_to_cart(self):
         """加入第一個商品到購物車"""
-        self.click(".inventory_item button")
+        self.click("#add-to-cart-sauce-labs-backpack")
 
     def go_to_cart(self):
         """前往購物車"""
