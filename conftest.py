f09daa2ca005938944856b54a6f5461da16e2bf1
Joe <joegou721@gmail.com>
2026-04-23 12:44:09 +0800

    fix: yml 縮排錯誤 & conftest.py 設定無頭模式

--- conftest.py
+++ conftest.py
@@ -20,20 +20,21 @@ def pytest_addoption(parser):
 @pytest.fixture
 def page(request):
     slow = request.config.getoption("--slow")
+    # 👉 關鍵：從 pytest 的參數抓取 headless 設定 (預設是 True)
+    # 如果你下指令時沒加 --headed，在 CI 就會自動跑 headless
+    is_headless = not request.config.getoption("--headed")
 
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            headless=False,
+            headless=is_headless,  # 改為動態判斷
             slow_mo=2000 if slow else 0
         )
 
-        # 👉 設定畫面大小
+        # 設定畫面大小
         context = browser.new_context(
             viewport={"width": 1920, "height": 1080}
         )
 
         page = context.new_page()
-
         yield page
-
         browser.close()
